be76bb9d9dd0d836fdafcca68e0a832efa122738
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-21 12:32:32 +0530

    Create reduction_op.sv

---
+++ Operators/reduction_op.sv
@@ -0,0 +1,28 @@
+module reduction_code;
+logic [0:3]a,y;
+
+initial begin
+a=4'b1010;
+
+y=!a;
+$display("\n \t the reduction output of NOT is %d ",y);
+
+y=|a;
+$display("\n \t the reduction output of OR is %0b ",y);
+
+y=&a;
+$display("\n \t the reduction output of AND is %0d ",y);
+
+y=~|a;
+$display("\n \t the reduction output of NOR is %0b ",y);
+
+y=~&a;
+$display("\n \t the reduction output of NAND is %0b ",y);
+
+y=^a;
+$display("\n \t the reduction output of XOR is %0b ",y);
+
+y=~^a;
+$display("\n \t the reduction output of XNOR is %0b ",y);
+end
+endmodule 
