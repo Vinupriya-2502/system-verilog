84149d20607748733c984a9afbfcee3cf67a53f6
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-21 14:27:24 +0530

    Create int_to_longint.sv

---
+++ Casting_Data_types/int_to_longint.sv
@@ -0,0 +1,18 @@
+module int_to_longint();
+
+ int int_data; 
+ longint longint_data;
+
+ initial begin
+
+   int_data = 8'b11110011; 
+
+   $display("\nsize of int = %0d",$size(int_data));
+   $display("before casting int_data = %b",int_data);
+
+   longint_data = longint'(int_data); 
+
+   $display("after casting int to longint = %b\n",longint_data);
+
+ end
+endmodule : int_to_longint
