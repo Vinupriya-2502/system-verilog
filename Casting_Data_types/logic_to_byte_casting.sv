8b1c0c381142d3a773dafe6d640618c719b2632c
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-21 14:45:15 +0530

    Create logic_to_byte_casting.sv

---
+++ Casting_Data_types/logic_to_byte_casting.sv
@@ -0,0 +1,10 @@
+module cast_logic_byte();
+ logic [3:0] logic_data;
+ byte byte_data;
+ initial begin
+   logic_data = 4'b1xx1; 
+   $display("\nbefore casting logic_data = %0b",logic_data);
+   byte_data = byte'(logic_data); 
+   $display("after casting logic to byte = %b\n",byte_data);
+ end
+endmodule : cast_logic_byte
