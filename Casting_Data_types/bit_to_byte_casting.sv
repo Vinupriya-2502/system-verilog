763435b7407ae18a59e2700ffbe64a8534a0cbf1
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-21 13:24:37 +0530

    Create bit_to_byte_casting.sv

---
+++ Casting_Data_types/bit_to_byte_casting.sv
@@ -0,0 +1,17 @@
+module bit_to_byte();
+ 
+ byte byte_data;
+ bit [3:0] bit_data;
+
+ initial begin
+
+   bit_data = 4'b1011; 
+
+   $display("\nbefore casting bit_data = %0b",bit_data);
+
+   byte_data = byte'(bit_data); 
+
+   $display("after casting bit to byte = %b\n",byte_data);
+
+ end
+endmodule : bit_to_byte
