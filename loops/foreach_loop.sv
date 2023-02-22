cdea366a17f5156f088c6d5e472e7780a4c78d86
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-22 22:08:01 +0530

    Create foreach_loop.sv

---
+++ loops/foreach_loop.sv
@@ -0,0 +1,11 @@
+module foreach_loop;
+  int array[5];
+  initial 
+  begin
+    foreach(array[i])
+    begin 
+      array[i]=i;
+      $display("\tarray[%0d]=%0d",i,array[i]);
+    end  
+  end 
+endmodule
