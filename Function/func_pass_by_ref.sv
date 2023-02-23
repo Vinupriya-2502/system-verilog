7053b47ed7477cd29402431b48d9dfc2eae7dd2a
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-02-23 13:20:27 +0530

    Create func_pass_by_ref.sv

---
+++ Function/func_pass_by_ref.sv
@@ -0,0 +1,33 @@
+module func_pass_by_ref;
+  int result,addend,augend;
+  initial
+  begin
+    addend=5;
+    augend=6;
+    result=sum_without_ref(addend,augend);
+    $display("\tafter calling function without ref -> addend = %0d, augend =%0d",addend,augend);
+    result=sum_with_ref(addend,augend);
+    $display("\tafter calling function with ref -> addend = %0d, augend =%0d",addend,augend);
+  end
+  function automatic int sum_with_ref(ref int var1,var2);
+    int temp;
+    temp=var1;
+    var1=var2;
+    var2=temp;
+    $display("\tswapped variables by using ref ");
+    return var1+var2;
+  endfunction 
+
+  function int sum_without_ref(input int var1,var2);
+    int temp;
+    temp=var1;
+    var1=var2;
+    var2=temp;
+    $display("\tswapped variables by without using ref ");
+    return var1+var2;
+  endfunction : sum_without_ref
+
+endmodule 
+
+
+
