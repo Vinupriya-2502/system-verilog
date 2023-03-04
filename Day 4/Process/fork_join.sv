f1d88e3821f046f50463d2087f1631c4d0f5c91a
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-04 18:38:00 +0530

    Update fork_join.sv

--- Day 4/Process/fork_join.sv
+++ Day 4/Process/fork_join.sv
@@ -1,26 +1,25 @@
-module fork_join();
-  event e1;
-  int a=5,b=6,c,d;
-  initial begin  
-    $display("[%0t] Thread_T1: Values of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d); 
+module fork_join;
+
+  initial begin
+    $display("-------------------------------------------");
     fork
+      //Process-1
       begin
-        #1 a <= b;
-        b <= 7;
-        $monitor("[%0t] Thread_T2: Values of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d);
-        #1 ->e1;
-        c = b;
+        $display($time,"\tProcess-1 Started");
+        #5;
+        $display($time,"\tProcess-1 Finished");
       end
+
+      //Process-2
       begin
-        wait(e1.triggered);
-        $display("[%0t] Event is triggered",$time);        
-        begin
-          #1 d = c;
-          b <= 1;
-        end
+        $display($time,"\tProcess-2 Started");
+        #20;
+        $display($time,"\tProcess-2 Finished");
       end
-    join  
-    $display("[%0t] Thread_T3: value of a= %0d,b= %0d, c= %0d,d= %0d",$time,a,b,c,d);
-   end
-endmodule:fork_join
+    join
+    $display($time,"\tOutside Fork-Join");
+    $display("-------------------------------------------");
+    $finish;
+  end
+endmodule
 
