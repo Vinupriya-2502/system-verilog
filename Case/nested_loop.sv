20e92f634f9518593a1eaf5b60ed590da6d11fa1
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-05 19:28:35 +0530

    Create nested_loop.sv

---
+++ Case/nested_loop.sv
@@ -0,0 +1,20 @@
+module nested_loop;
+  initial 
+  begin
+    nes();
+  end
+
+  task nes();
+      for (int i=1;i<=3;i++)
+      begin
+        for(int j=1;j<=3;j++)
+        begin
+          if(i==2)
+          begin
+            disable nes;
+          end
+          $display("\t i= %0d , j= %0d ",i,j);
+        end
+      end
+    endtask
+endmodule:nested_loop
