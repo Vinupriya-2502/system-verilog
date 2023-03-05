b9a233d1a13ed7f37647bbb9262514f67c9dce62
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-05 19:23:16 +0530

    Create priority_if4.sv

---
+++ if variants/priority_if4.sv
@@ -0,0 +1,26 @@
+module priorityif_4;
+int bill;
+initial begin
+bill = 6000;
+$display ("Total bill = %0d",bill);
+$display ("------------------------");
+priority if (bill < 1000)begin
+$display ("Inside the priority if ");
+$display ("No discount");
+end
+else if (bill ==8000)begin
+$display ("Inside the first else if block ");
+$display ("10 percent discount available ");
+end
+else if (bill >8000)begin
+$display ("Inside the second else if block ");
+$display ("15 percent discount available");
+end
+else begin
+$display ("Inside the else block ");
+$display ("5 percent discount available ");
+end
+$display ("Out from the conditional block");
+$display ("Do more shopping for more discount ......");
+end
+endmodule
