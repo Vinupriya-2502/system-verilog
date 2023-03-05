787ce5866f4471ec0ca031d7ec16012456bee61e
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-05 20:05:18 +0530

    Create unique_case_none.sv

---
+++ Case/unique_case_none.sv
@@ -0,0 +1,13 @@
+module top;
+bit [1:0] x;
+initial begin
+  x = 2'b10;
+
+ unique case(x)
+  00 : $display(" Value of x is = %0b", x);
+  01 : $display(" Value of x is = %0b", x);
+  //10 : $display(" Value of x is = %0b", x);
+  11 : $display(" Value of x is = %0b", x);
+endcase
+end
+endmodule
