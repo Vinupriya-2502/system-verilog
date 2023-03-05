70c46383c66972ccc17509e45e65178f25af4ea7
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-05 19:31:41 +0530

    Create priority_case.sv

---
+++ Case/priority_case.sv
@@ -0,0 +1,11 @@
+module top;
+bit [2:0] pqr;
+initial begin
+  pqr = 5;
+  priority case (pqr)
+5 : $display ("Found to be 5");
+5 : $display ("Again found to be 5");
+7 : $display ("Found to be 7");
+endcase
+end
+endmodule
