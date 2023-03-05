5cb418a07fd6d9651faec2512e92598ede986a57
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-05 19:27:01 +0530

    Create break.sv

---
+++ Case/break.sv
@@ -0,0 +1,19 @@
+module top;
+bit [1:0] x;
+
+initial begin
+  x = 2'b01;
+    case(x)
+      00 : $display("Value of x = %0b", x);
+      01 :begin
+          $display("Value of x = %0b",x);
+          break;
+           end
+      10 : $display("Value of x = %0b",x);
+      11 : $display("Value of x = %0b" ,x);
+      default : $display("Value of x is not find");
+    endcase
+  end
+  endmodule
+
+
