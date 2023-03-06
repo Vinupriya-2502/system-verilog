ecdbb57a53a47d03afcc768d039dbf94a4e349b5
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-06 13:09:05 +0530

    Update solve_before.sv

--- Day 5/solve_before.sv
+++ Day 5/solve_before.sv
@@ -1,4 +1,5 @@
 class without_solve_before;
+  
   rand bit value1;
   rand bit [3:0] value2;
   
@@ -13,6 +14,7 @@ class without_solve_before;
 endclass
 
 class with_solve_before;
+  
   rand bit value1;
   rand bit [3:0] value2;
   
@@ -23,7 +25,7 @@ class with_solve_before;
                      if(value1==1)
                        $write("%c[1;31m",27);
                        else
-                        $write("%c",27);
+                        $write("%c[0m",27);
                   endfunction
 
 endclass
@@ -32,20 +34,23 @@ module solve_before();
 
    without_solve_before gen1 = new();
    with_solve_before gen2 = new();
+
   initial
   begin
     $display("\t------Without solve before----------");
     for(int i=1;i<=32;i++)
     begin
         void'(gen1.randomize());
-        #1 $display("\t[%0t] @ iteration: %0d -----> value1: %0d \t value2: %0d",$time,i,gen1.value1,gen1.value2,27);
+        #1 $display("\t[%0t] @ iteration: %0d -----> value1: %0d \t value2: %0d%c[0m",$time,i,gen1.value1,gen1.value2,27);
     end
     $display("\n\t -----with solve before--------");
     for(int i=1;i<=10;i++)
     begin
         void'(gen2.randomize());
-        #1 $display("\t[%0t] @ iteration: %0d -----> value1: %0d \t value2: %0d",$time,i,gen2.value1,gen2.value2,27);
+        #1 $display("\t[%0t] @ iteration: %0d -----> value1: %0d \t value2: %0d%c[0m",$time,i,gen2.value1,gen2.value2,27);
     end
+
   end
 
 endmodule : solve_before
+
