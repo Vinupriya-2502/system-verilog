51227c187109a2a07036c94660db23729d14e9de
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-07 00:16:38 +0530

    Create inside_cons.sv

---
+++ Day 5/implication/inside_cons.sv
@@ -0,0 +1,17 @@
+class PQR; 
+  rand bit [3:0] var1;
+  constraint C1 {(var1 inside {[3:9]});}
+endclass
+  
+module top;
+initial begin
+int i;
+  PQR pqr;
+  pqr=new();
+  for(int i =1; i<7;i++)
+  begin
+    void'(pqr.randomize());
+    $display("[%0t] iteration: %0d -----> var1=%0d",$time,i,pqr.var1);
+  end
+end
+endmodule
