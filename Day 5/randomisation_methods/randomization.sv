fdf776d35831ff542f40d9b1215e277521a6b4ae
Vinupriya-2502 <113250417+Vinupriya-2502@users.noreply.github.com>
2023-03-07 13:11:14 +0530

    Create randomization.sv

---
+++ Day 5/randomisation_methods/randomization.sv
@@ -0,0 +1,19 @@
+class generator;
+  rand bit [2:0] value;
+  constraint cons {value==5;}
+endclass
+
+module randomization();
+   generator gen = new();
+  initial
+  begin
+      if(gen.randomize())
+        $display("\tvalue: %0d \tRandomization successful",gen.value);
+      else
+        $display("\tvalue: %0d \tRandomization Failed",gen.value);
+      if(gen.randomize()with{value==2;})
+        $display("\tvalue: %0d \tRandomization successful",gen.value);
+      else
+        $display("\tvalue: %0d \tRandomization Failed",gen.value);
+  end
+endmodule
